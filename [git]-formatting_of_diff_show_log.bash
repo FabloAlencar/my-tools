8e90383e267bcf7f81ed9d53bc371523503aa861
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2018-10-30 21:30:29 +0100

    Added aliases files.

---
+++ [git]-formatting_of_diff_show_log.bash
@@ -0,0 +1,3 @@
+git config --global alias.dp "diff --word-diff --unified=10" 
+git config --global alias.so "show --pretty='parent %Cred%p%Creset commit %Cred%h%Creset%C(yellow)%d%Creset%n%n%w(72,2,2)%s%n%n%w(72,0,0)%C(cyan)%an%Creset %Cgreen%ar%Creset'" 
+git config --global alias.lg "log --pretty='%Cred%h%Creset | %C(yellow)%d%Creset %s %Cgreen(%cr)Creset %C(cyan)[%an]%Creset' --graph"
